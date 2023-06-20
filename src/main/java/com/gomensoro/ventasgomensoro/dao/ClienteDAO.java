bdfe20a929dec15a58046582f0ce336411dd743c
Gonzalo Cozzo <gonza007_88@hotmail.com>
2023-06-19 22:04:00 -0300

    creacion de los metodos de Cliente.Dao

--- src/main/java/com/gomensoro/ventasgomensoro/dao/ClienteDAO.java
+++ src/main/java/com/gomensoro/ventasgomensoro/dao/ClienteDAO.java
@@ -1,39 +1,134 @@
-
 package com.gomensoro.ventasgomensoro.dao;
 
+import com.gomensoro.ventasgomensoro.acceso.Acceso;
 import com.gomensoro.ventasgomensoro.dao.interfaces.metodosDao;
+import com.gomensoro.ventasgomensoro.entidades.Cliente;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
+import javax.swing.JOptionPane;
 
+public class ClienteDAO implements metodosDao<Cliente> {
 
-public class ClienteDAO implements metodosDao{
-
+    private final List<Cliente> lista;
+    private Metodos<Cliente> metodos;
     private final String ruta = "cliente.txt";
-    private boolean resp;
-    
+    private boolean resp; //creado para saber si se inserto con exito o no un metodo
+    private Cliente cliente;
+
+    public ClienteDAO() {
+        lista = new ArrayList<>();
+        metodos = new Metodos<>(lista);
+        cargarLista(); //cada vez que se inicialice se va a cargar la lista con la lectura del archivo txt
+    }
+
+    private void cargarLista() {
+        Cliente cliente;
+        for (String dato : Acceso.cargarArchivo(ruta)) {
+            StringTokenizer st = new StringTokenizer(dato, ","); //sirve para leer los datos separados en este caso por una ","
+            cliente = new Cliente(Integer.parseInt(st.nextToken()), st.nextToken());
+            metodos.agregarRegistro(cliente);
+        }
+    }
+
     @Override
     public List listar() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        List<Cliente> registros = new ArrayList<>();
+        try {
+            for (String dato : Acceso.cargarArchivo(ruta)) {
+                StringTokenizer st = new StringTokenizer(dato, ","); //sirve para leer los datos separados en este caso por una ","
+                cliente = new Cliente(Integer.parseInt(st.nextToken()), st.nextToken());
+                registros.add(cliente);
+            }
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Error al listar Clientes: " + e.getMessage());
+        }
+
+        return registros;
     }
 
     @Override
-    public boolean insertar(Object obj) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public boolean insertar(Cliente obj) {
+        resp = false;
+        PrintWriter pw;
+        FileWriter fw;
+
+        try {
+            fw = new FileWriter("Archivos/" + ruta); //donde vamos a guardar la informacion
+            pw = new PrintWriter(fw); //escribe en el archivo segun la ruta que le pasemos
+            obj = new Cliente(obj.getRut(), obj.getNombre());
+            int codigo = buscarCodigo(obj.getRut());
+
+            if (codigo == -1) {
+                metodos.agregarRegistro(obj);
+            } else {
+                metodos.modificar(codigo, obj);
+            }
+
+            for (int i = 0; i < metodos.cantidadRegistro(); i++) {
+                cliente = metodos.obtenerRegistro(i);
+                pw.println(String.valueOf(cliente.getRut() + "," + cliente.getNombre()));
+            }
+            pw.close();
+            resp = true;
+
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Error al modificar Clientes: " + e.getMessage());
+        }
+
+        return resp;
     }
 
     @Override
-    public boolean actualizar(Object obj) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public boolean actualizar(Cliente obj) {
+        resp = false;
+        PrintWriter pw;
+        FileWriter fw;
+
+        try {
+            fw = new FileWriter("Archivos/" + ruta); //donde vamos a guardar la informacion
+            pw = new PrintWriter(fw); //escribe en el archivo segun la ruta que le pasemos
+            obj = new Cliente(obj.getRut(), obj.getNombre());
+            metodos.agregarRegistro(obj);
+
+            for (int i = 0; i < metodos.cantidadRegistro(); i++) {
+                cliente = metodos.obtenerRegistro(i);
+                pw.println(String.valueOf(cliente.getRut() + "," + cliente.getNombre()));
+            }
+            pw.close();
+            resp = true;
+
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Error al insertar Clientes: " + e.getMessage());
+        }
+
+        return resp;
     }
 
     @Override
     public int buscarCodigo(int codigo) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        for (int i = 0; i < metodos.cantidadRegistro(); i++) {
+            if (codigo == metodos.obtenerRegistro(i).getRut()) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
-    public Object getObjeto(int codigo) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public Cliente getObjeto(int codigo) {
+        Cliente cliente = null;
+
+        for (int i = 0; i < metodos.cantidadRegistro(); i++) {
+            cliente = metodos.obtenerRegistro(i);
+            if (cliente.getRut() == codigo) {
+                return cliente;
+            }
+        }
+        return cliente;
     }
-    
-    
+
 }
